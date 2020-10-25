84a1900d4acb3c0b2889236ca3c4fca3e1c69567
leslie-alldridge <leslie.alldridge@gmail.com>
2020-10-25 18:56:29 +1300

    Chapter 2 strings

---
+++ Chapter 2/Basics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Reflection;
+using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In line comment
+
+            /* Multi line
+            comment
+            */
+            double heightInMetres = 1.88;
+            char symbol = '$';
+
+            // Verbatim string literal used to escape \t
+            string filePath = @"C:\televisions\sony\bravia.txt";
+
+
+            Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
+            
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+                int methodCount = 0;
+
+                foreach (var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+                }
+
+                Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+                arg0: a.DefinedTypes.Count(),
+                arg1: methodCount,
+                arg2: r.Name);
+            }
+        }
+    }
+}
---
+++ Chapter 2/Notes.md
@@ -0,0 +1,14 @@
+Keywords such as **string** that look like types are aliases, which represent types provided by the platform on which C# runs.
+
+Casing:
+- camelCase for local variables and private fields
+- TitleCase for non-private fields and methods
+
+• Literal string: Characters enclosed in double-quote characters. They can use
+escape characters like \t for tab.
+
+• Verbatim string: A literal string prefixed with @ to disable escape characters
+so that a backslash is a backslash.
+
+• Interpolated string: A literal string prefixed with $ to enable embedded
+formatted variables. You will learn more about this later in this chapter.
