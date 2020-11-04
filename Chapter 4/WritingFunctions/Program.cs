6f82a13dae0824bd2808cc9d9cedf3905ca21746
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-04 19:10:26 +1300

    cardinal to original

--- Chapter 4/WritingFunctions/Program.cs
+++ Chapter 4/WritingFunctions/Program.cs
@@ -96,10 +96,54 @@ namespace WritingFunctions
             }
         }
 
+        static string CardinalToOriginal(int number)
+        {
+            switch (number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+                default:
+                    string numberAsText = number.ToString();
+                    char lastDigit = numberAsText[numberAsText.Length - 1];
+                    string suffix = string.Empty;
+                    switch (lastDigit)
+                    {
+                        case '1':
+                            suffix = "st";
+                            break;
+                        case '2':
+                            suffix = "nd";
+                            break;
+                        case '3':
+                            suffix = "rd";
+                            break;
+                        default:
+                            suffix = "th";
+                            break;
+                    }
+                    return $"{number}{suffix}";
+            }
+        }
+
+        static void RunCardinalToOriginal()
+        {
+            for (int number = 1; number <= 40; number++)
+            {
+                Write(CardinalToOriginal(number));
+                if (number != 40)
+                {
+                    Write(", ");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            RunTimesTable();
-            RunCalculateTax();
+            // RunTimesTable();
+            // RunCalculateTax();
+            RunCardinalToOriginal();
         }
     }
 }
