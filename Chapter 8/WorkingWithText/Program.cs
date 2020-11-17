99b1faa0862b45d72fae74f88c0b510ddae9f58a
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-18 10:55:40 +1300

    WorkingWithText

---
+++ Chapter 8/WorkingWithText/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "London";
+            WriteLine($"{city} is {city.Length} characters long.");
+            WriteLine($"First char is {city[0]} and third is {city[2]}.");
+
+            // splitting a string
+            string cities = "Paris,Berlin,Madrid,New York";
+
+            string[] citiesArray = cities.Split(',');
+            foreach (string cityName in citiesArray)
+            {
+                WriteLine(cityName);
+            };
+        }
+    }
+}
