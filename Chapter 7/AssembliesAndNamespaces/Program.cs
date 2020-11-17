3011b2be0129309b9854fceab7e8bf72057f7aea
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-17 22:02:20 +1300

    Nuget restore, packages and more

---
+++ Chapter 7/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+using System.Xml.Linq;
+using Packt.Shared;
+using DialectSoftware.Collections;
+using DialectSoftware.Collections.Generics;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XDocument();
+            string s1 = "Hello"; // best practice is to use lowercase "string" so we don't have to import the System namespace
+            String s2 = "World";
+            WriteLine(s1 + s2);
+
+            var x = new Axis("x", 0, 10, 1);    
+            var y = new Axis("y", 0, 4, 1);
+
+            Write("Enter a color value in Hex: ");
+            string hex = ReadLine();
+
+            WriteLine("Is {0} a valid color value? {1}", arg0: hex, arg1: hex.IsValidHex());
+
+            Write("Enter a XML tag: ");
+            string xmlTag = ReadLine();
+            WriteLine("Is {0} a valid XML tag? {1}",
+             arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+            Write("Enter a password: ");
+            string password = ReadLine();
+            WriteLine("Is {0} a valid password? {1}",
+             arg0: password, arg1: password.IsValidPassword());
+        }
+    }
+}
--- Chapter 7/Readme.md
+++ Chapter 7/Readme.md
@@ -5,3 +5,27 @@ This chapter covers the following topics:
 - Decompiling assemblies
 - Packaging your libraries for NuGet distribution
 - Porting from .NET Framework to .NET Core
+
+Some of the more important packages
+- System.Runtime (Object, string, Int32, Array)
+- System.Collections (List<t>, Dictionary<TKey, TValue>)
+- System.Net.Http (HttpClient, HttpResponseMessage)
+- System.IO.Filesystem (File, Directory)
+- System.Reflection (Assembly, TypeInfo, MethodInfo)
+
+![Type Keywords](typekeywords.PNG)
+
+Press F12 to go to definition. You may notice the namespace and assembly names differ.
+
+There are three ways to publish and deploy a .Net Core application. There are:
+- Framework-dependent deployment (FDD)
+- Framework-dependent executables (FDEs)
+- Self-contained (includes .Net Core)
+
+You can use the RID (Runtime IDentifier) catalog to identify currently supported values.
+
+To build and release code as a `nupkg` file do the following:
+- `dotnet build -c Release`
+- `dotnet pack -c Release`
+
+![Differences](differences.PNG)
