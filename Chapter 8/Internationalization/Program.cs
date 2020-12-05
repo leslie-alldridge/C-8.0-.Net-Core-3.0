7f95bd595dec3bbe586e4d03c61be3ff19e0e970
leslie-alldridge <leslie.alldridge@gmail.com>
2020-12-05 16:34:09 +1300

    Globalization

--- Chapter 8/Internationalization/Program.cs
+++ Chapter 8/Internationalization/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using static System.Console;
 
 namespace Internationalization
 {
@@ -7,7 +8,34 @@ namespace Internationalization
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo globalization = CultureInfo.CurrentCulture;
+            CultureInfo localization = CultureInfo.CurrentUICulture;
+
+            WriteLine("The current globalization culture is {0}: {1}", arg0: globalization.Name, arg1: globalization.DisplayName);
+            WriteLine("The current localization culture is {0}: {1}", arg0: localization.Name, arg1: localization.DisplayName);
+
+            WriteLine("en-US: English (United States)");
+            WriteLine("da-DK: Danish (Denmark)");
+            WriteLine("fr-CA: French (Canada)");
+            Write("Enter an ISO culture code: ");
+            string newCulture = ReadLine();
+            if (!string.IsNullOrEmpty(newCulture))
+            {
+                var ci = new CultureInfo(newCulture);
+                CultureInfo.CurrentCulture = ci;
+                CultureInfo.CurrentUICulture = ci;
+            }
+            WriteLine();
+            Write("Enter your name: ");
+            string name = ReadLine();
+            Write("Enter your date of birth: ");
+            string dob = ReadLine();
+            Write("Enter your salary: ");
+            string salary = ReadLine();
+            DateTime date = DateTime.Parse(dob);
+            int minutes = (int)DateTime.Today.Subtract(date).TotalMinutes;
+            decimal earns = decimal.Parse(salary);
+            WriteLine($"{name} was born on a {date:dddd}, is {minutes:N0} minutes old, and earns { earns:C}");
         }
     }
 }
